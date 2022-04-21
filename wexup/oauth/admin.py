aef3b7c84112a8bad08a486c4142e129ef056a32
Ilyas Zinollin <zinollin2000@gmail.com>
2022-04-21 23:53:02 +0600

    created student and recruiter models

--- wexup/oauth/admin.py
+++ wexup/oauth/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Student, Recruiter
+
+admin.site.register(Student)
+admin.site.register(Recruiter)
 
 # Register your models here.
--- wexup/oauth/models.py
+++ wexup/oauth/models.py
@@ -1,4 +1,31 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
+class Student(models.Model):
+    email = models.EmailField(max_length=60, blank=False, null=False, unique=True, verbose_name="email")
+    name = models.CharField(max_length=30, blank=False, null=False, verbose_name="name")
+    surname = models.CharField(max_length=30, blank=False, null=False, verbose_name="surname")
+    password = models.CharField(max_length=260, blank=False, null=False, verbose_name="password")
+    university = models.CharField(max_length=100, blank=False, null=False, verbose_name="university")
+    city = models.CharField(max_length=60, blank=False, null=False, verbose_name="city")
+    study_period = models.CharField(max_length=30, blank=False, null=False, verbose_name='study period')
+    specialty = models.CharField(max_length=100, blank=False, null=False, verbose_name='specialty')
+    gender = models.CharField(max_length=20, blank=True, null=False, verbose_name='gender')
+    avatar = models.ImageField(blank=True, null=True, upload_to='images/')
+    resume = models.FileField(blank=True, null=True, upload_to='resumes/')
+    favored_roles = models.CharField(blank=False, null=False, max_length=200, verbose_name='favored roles')
+
+    def __str__(self):
+        return f"{self.email}"
+
+
+class Recruiter(models.Model):
+    email = models.EmailField(max_length=60, blank=False, null=False, unique=True, verbose_name="email")
+    name = models.CharField(max_length=30, blank=False, null=False, verbose_name="name")
+    surname = models.CharField(max_length=30, blank=False, null=False, verbose_name="surname")
+    password = models.CharField(max_length=260, blank=False, null=False, verbose_name="password")
+    company = models.CharField(max_length=60, blank=False, null=False, verbose_name='company')
+    position = models.CharField(max_length=60, blank=False, null=False, verbose_name='position')
+
+    def __str__(self):
+        return f"{self.email}"
