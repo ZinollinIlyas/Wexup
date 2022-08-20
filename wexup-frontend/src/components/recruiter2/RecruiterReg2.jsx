bb433373160b1fd93525ffa9150a656bea5e7b62
Ilyas Zinollin <zinollin2000@gmail.com>
2022-08-20 18:43:23 +0600

    changed url for api call

--- wexup-frontend/src/components/recruiter2/RecruiterReg2.jsx
+++ wexup-frontend/src/components/recruiter2/RecruiterReg2.jsx
@@ -34,14 +34,14 @@ const RecruiterReg2 = () =>
             },
             body: formdata
         }
-        let response = await fetch("http://194.110.55.147:8000/api/users/recruiters/", options);
+        let response = await fetch(`http://${process.env.REACT_APP_API_URL}:8000/api/users/recruiters/`, options);
         let data = await response.json();
         if (response.status === 200) {
             console.log(data);
             let another_formData = new FormData();
             another_formData.append("email", data.email)
             another_formData.append("password", localStorage.getItem("password"))
-            let another_response = await fetch("http://194.110.55.147:8000/api/token/", {
+            let another_response = await fetch(`http://${process.env.REACT_APP_API_URL}/api/token/`, {
                 method: "POST",
                 header: {
                 "Content-Type": "application/json"
